6e5d30608721f808ee7d8f5b7b6e9c8fd35d07d1
deaputri27 <deaputri0827@gmail.com>
2023-06-28 16:55:40 +0700

    express hello world

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
 
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
